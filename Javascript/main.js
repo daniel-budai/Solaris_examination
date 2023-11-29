d7339ffc237c529f83d8895534b8fc70530ab760
daniel-budai <danielbudai@tutanota.com>
2023-11-29 16:42:15 +0100

    Progress, redid openoverlay function, added dom-manipulation, html ID, continue CSS

--- Javascript/main.js
+++ Javascript/main.js
@@ -9,11 +9,37 @@ let jupiterButton = document.getElementById("jupiter");
 let saturnButton = document.getElementById("saturn");
 let uranusButton = document.getElementById("uranus");
 let neptuneButton = document.getElementById("neptune");
+let planets = [];
 //#endregion
 
 // #region Overlay
-const openOverlay = () =>
-  (document.getElementById("overlay").style.display = "flex");
+
+const openOverlay = function (planet) {
+  console.log(planet);
+  document.getElementById("overlay").style.display = "flex";
+  document.getElementById("latin-name").innerText = planet.latinName;
+  document.getElementById("planet-name").innerText = planet.name;
+  document.getElementById("planet-description").innerText = planet.desc;
+  document.getElementById(
+    "planet-circumference"
+  ).innerText = `Circumference: ${planet.circumference}`;
+  document.getElementById(
+    "planet-distance"
+  ).innerText = `Distance: ${planet.distance}`;
+  document.getElementById(
+    "planet-maxTemp"
+  ).innerText = `Max Temperature: ${planet.maxTemp}`;
+  document.getElementById(
+    "planet-minTemp"
+  ).innerText = `Min Temperature: ${planet.minTemp}`;
+  document.getElementById("planet-moons").innerText = `Moons: ${planet.moons}`;
+
+  // Display the overlay
+  document.getElementById("overlay").style.display = "flex";
+};
+
+//const openOverlay = () =>
+//(document.getElementById("overlay").style.display = "flex");
 
 const closeOverlay = () =>
   (document.getElementById("overlay").style.display = "none");
@@ -22,15 +48,34 @@ const addClickListener = (button, callback) =>
   button.addEventListener("click", callback);
 // #endregion
 
-addClickListener(sunButton, openOverlay);
-addClickListener(mercuryButton, openOverlay);
-addClickListener(venusButton, openOverlay);
-addClickListener(earthButton, openOverlay);
-addClickListener(marsButton, openOverlay);
-addClickListener(jupiterButton, openOverlay);
-addClickListener(saturnButton, openOverlay);
-addClickListener(uranusButton, openOverlay);
-addClickListener(neptuneButton, openOverlay);
+addClickListener(sunButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Solen"))
+);
+addClickListener(mercuryButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Merkurius"))
+);
+addClickListener(venusButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Venus"))
+);
+addClickListener(earthButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Jorden"))
+);
+addClickListener(marsButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Mars"))
+);
+addClickListener(jupiterButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Jupiter"))
+);
+addClickListener(saturnButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Saturnus"))
+);
+addClickListener(uranusButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Uranus"))
+);
+addClickListener(neptuneButton, () =>
+  openOverlay(planets.find((planet) => planet.name === "Neptunus"))
+);
+
 addClickListener(document.getElementById("x"), closeOverlay);
 
 async function getKey() {
@@ -60,4 +105,10 @@ async function getPlanets(key) {
     console.log(error);
   }
 }
-getKey().then((key) => getPlanets(key));
+getKey()
+  .then((key) => getPlanets(key))
+  .then((bodies) => (planets = bodies));
+
+//get element by id
+//innertext = värde planets.circum
+//innan flex
