4d396dcad1ffb4ca28548d538d20caf8113fc8c9
daniel-budai <danielbudai@tutanota.com>
2023-12-06 13:36:55 +0100

    comments

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,6 +10,14 @@ let uranusButton = document.getElementById("uranus");
 let neptuneButton = document.getElementById("neptune");
 let planets = [];
 
+/* comment
+Hämtar innehåller i planet arrayen. och skriver ut det med .innerText planet.name - alltså namnet på planeten. name: Jorden
+alla get element by ID gör det. I javascripten style:ar jag resultatet, egentligen kan man göra det i CSS och call på det här i JS
+men jag ville testa göra det såhär för att se hur det blir. 
+Eftersom jag ville ha det identiskt till figman skapade jag en funktion för moons som sätter komma-tecken efter varje måne och en punkt
+i slutet.  Loopar igenom varje måne (moon) i arrayen planet.moons 
+End comment */
+
 const openOverlay = function (planet) {
   document.getElementById("latin-name").innerText = planet.name;
   document.getElementById("planet-name").innerText = planet.latinName;
@@ -30,7 +38,11 @@ const openOverlay = function (planet) {
   });
   generateStars();
 };
-
+/* comment
+Hanterar stjärnorna i overlay. Jag ville göra stjärnorna identiska till Figman, fick det till 36 stjärnorn, kan vara fler. ?
+Skapar 36 stjärnor med en loop. För varje iteration skapas ett nytt div-element unikt ID star.id = `star-${i + 1}`; , 
+och läggs till i starsContainer. 
+End comment */
 const generateStars = () => {
   const starsContainer = document.getElementById("stars-container");
   starsContainer.innerHTML = "";
@@ -43,12 +55,25 @@ const generateStars = () => {
   }
 };
 
+/* comment
+ closeOverlay - för att stänga ner overlay 
+ Callback funktion för att öppna overlayen
+End comment */
+
 const closeOverlay = () => (document.getElementById("overlay").style.display = "none"); // prettier-ignore
 
 const addClickListener = (button, callback) => button.addEventListener("click", callback); // prettier-ignore
 
-const planetContainer = document.getElementById("planet-container");
+const planetContainer = document.getElementById("planet-container"); //planetContainer fungerar som behållare planetinformationen
 
+/* comment
+Ändra klassnamnet på planet-container, nollställ classNamnet med tom sträng, sedan .add för att lägga till nya
+används för att ändra stilen  av planetContainer baserat på vilken planet som visas. 
+Lägger sedan till eventlisteners på de olika knapparna, varje clickevent ändrar stilen. Te.x om man öppnar venusButton, ändras stilen
+till venus-container. 
+Öppnar overlay med .find, är en metod för att hitta objektet i arrayen med samma namn med jämnförelseoperatorn. name: solen === "Solen" 
+anonymus functions
+End comment */
 const updatePlanetContainerStyles = (planetClassName) => {
   planetContainer.className = "";
   planetContainer.classList.add(planetClassName);
@@ -100,6 +125,19 @@ addClickListener(neptuneButton, () => {
 });
 
 addClickListener(document.getElementById("x"), closeOverlay);
+
+/* comment
+Skickar async POST-request till base_url med enpointen /keys för att få nycklen.
+Nyckeln i respone.json. 
+Om något fel skickas error ut i catch, Try and catch
+
+GET-request till base_url med enpointen /bodies. I headers: sätter jag key variabeln från tidigare request
+Om något fel sker, loggas error,  - Try and catch
+
+Anropar funktionerna, Först getKey() när nycklen är hämtad går den vidare till .then() som tar nyckeln som argument och
+skickar in i  getPlanets() med arguemnt går sedan vidare till json bodies och sparar ner den i variabeln planets.
+Man kan se det som en kedja, kallas för promise-chaining. 
+ End comment */
 
 async function getKey() {
   try {
